021ed1903c51b07dedc1f8aaca60e68a02b11415
Trippxp <mrkportufe@icloud.com>
2026-06-26 16:54:30 +0000

    Fix root provider and middleware for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Make sure this line is here
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        //
+    }
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+    }
 }
